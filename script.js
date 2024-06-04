cb0b598db01dbf17ac9e5c3e2a4db8d59e12df7d
antoinevignes <168306302+antoinevignes@users.noreply.github.com>
2024-06-04 12:49:57 +0200

    Added computer choice and human choice functions.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+const getComputerChoice = () => {
+  let result = Math.floor(Math.random() * 3);
+  if (result === 0) {
+    return "rock";
+  } else if (result === 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+};
+
+const getHumanChoice = () => {
+  prompt("Rock, Paper, or Scissors?");
+};
+
+console.log(getHumanChoice());
